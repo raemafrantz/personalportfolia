2c5dedb04e401c140619ea82ebbc5b14f8d5e85d
reama frantz <rfrantz@bu.edu>
2022-12-04 17:44:22 -0500

    second version

---
+++ experience.js
@@ -0,0 +1,10 @@
+const frontCard = document.querySelector(".inner-card");
+const frontCard2 = document.querySelector(".inner-card-2");
+
+frontCard.addEventListener("click", () => {
+    frontCard.classList.toggle("is-flipped");
+});
+frontCard2.addEventListener("click", () => {
+    frontCard2.classList.toggle("is-flipped");
+});
+
